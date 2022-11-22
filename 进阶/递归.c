d5a3627215f143117a93ba7f95e6c8998b567655
jlywsj <2284344923@qq.com>
2022-11-22 23:11:16 +0800

    作用域

---
+++ 进阶/递归.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void recursion(void);
+
+void recursion(void)
+{
+	static int count = 10;
+
+	printf("H1!\n");
+	if (--count) {
+		recursion();
+	}
+}
+
+int main()
+{
+	recursion();
+	return 0;
+}
