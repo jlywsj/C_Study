3ef71221c908cfda16080c67e91e66bd49255f9f
jly <2284344923@qq.com>
2022-11-28 20:40:58 +0800

    更新

--- 进阶/结构体.c
+++ 进阶/结构体.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
+
+struct A
+{
+    char a;
+    int b;
+    char c;
+} a = {'x', 520, '0'},c;
+
+struct B
+{
+    char a;
+    int b;
+    char c;
+};
+
 int main(void)
 {
-    struct A
-    {
-        char a;
-        int b;
-        char c;
-    } a = { 'x', 520, '0'};
-    
-    printf("size of a = %d\n",sizeof(a));
+
+
+    // struct B b;
+    // b = a; 报错，类型不同
+    c = a;
+
+    // printf("size of a = %d\n", sizeof(a));
+    // printf("size of c = %d\n",sizeof(c));
+
+    printf("a %p\n",&a);
+    printf("c %p\n",&c);
+    printf("%d",offset(c));
 
     return 0;
 }
---
+++ 进阶/结构体作为参数.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Book
+{
+    char title[128];
+	char author[40];
+	float price;
+	struct Date date;
+	char publisher[40];
+};
+
+
+int main(void)
+{
+
+    return 0;
+}
