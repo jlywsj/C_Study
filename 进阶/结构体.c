101f90d12a2a05c7dc5bfe8f0256f6c1ce550d2e
jly <2284344923@qq.com>
2022-11-29 21:52:12 +0800

    "结构体指针"

--- 进阶/结构体.c
+++ 进阶/结构体.c
@@ -1,34 +1,92 @@
 #include <stdio.h>
+#include <string.h>
 
+//struct A
+//{
+//    char a;
+//    int b;
+//    char c;
+//} a = {'x', 520, '0'},c;
+//
+//struct B
+//{
+//    char a;
+//    int b;
+//    char c;
+//};
+//
+//int main(void)
+//{
+//
+//
+//    // struct B b;
+//    // b = a; 报错，类型不同
+//    c = a;
+//
+//    // printf("size of a = %d\n", sizeof(a));
+//    // printf("size of c = %d\n",sizeof(c));
+//
+//    printf("a %p\n",&a);
+//    printf("c %p\n",&c);
+//    printf("%d",offset(c));
+//
+//    return 0;
+//}
 
-struct A
+struct Date
 {
-    char a;
-    int b;
-    char c;
-} a = {'x', 520, '0'},c;
-
-struct B
-{
-    char a;
-    int b;
-    char c;
+	unsigned year;
+	unsigned month;
+	unsigned day;
 };
+
+struct Book
+{
+	char title[128];
+	char author[40];
+	float price;
+	struct Date date;
+	char publisher[40];
+};
+
+
+struct Book getInput(struct Book book) {
+	printf("请输入书名：");
+	scanf("%s", book.title);
+	printf("请输入作者：");
+	scanf("%s", book.author);
+	printf("请输入售价：");
+	scanf("%f", &book.price);
+	printf("请输入出版日期：");
+	scanf("%d-%d-%d", &book.date.year, &book.date.month, &book.date.day);
+	printf("请输入出版社：");
+	scanf("%s", book.publisher);
+
+	return book;
+}
+
+void printBook(struct Book book) {
+	printf("书名 :%s\n",book.title);
+	printf("作者 :%s\n",book.author);
+	printf("售价 :%.2f\n",book.price);
+	printf("出版日期 : %d-%d-%d\n",book.date.year, book.date.month,book.date.day);
+}
+
 
 int main(void)
 {
+	static struct Book b1;
+	static struct Book b2;
 
+	printf("请录入第一本书的信息....\n");
+	b1 = getInput(b1);
 
-    // struct B b;
-    // b = a; 报错，类型不同
-    c = a;
+	printf("请录入第二本书的信息....\n");
+	b2 = getInput(b2);
 
-    // printf("size of a = %d\n", sizeof(a));
-    // printf("size of c = %d\n",sizeof(c));
+	printBook(b1);
+	putchar("\n");
+	printBook(b2);
 
-    printf("a %p\n",&a);
-    printf("c %p\n",&c);
-    printf("%d",offset(c));
-
-    return 0;
+	return 0;
 }
