39840b2e0338d7fb612fefc8194b7b319b7aaddb
jly <2284344923@qq.com>
2022-12-11 00:07:18 +0800

    分治求最大

--- 进阶/算法实验报告 分治_求最大.c
+++ 进阶/算法实验报告 分治_求最大.c
@@ -13,21 +13,25 @@
 
 int getMax(int* arr, int ptr, int len)
 {
-	if (ptr == len) {
+	int lmax = 0;
+	int rmax = 0;
+	if (len > ptr) {
+		lmax = getMax(arr, ptr, (ptr + len) / 2);
+		rmax = getMax(arr, (ptr + len) / 2 + 1, len); // 如果mid 不 + 1 的话，会陷入死递归例如 getMax(arr, 0 , 1)会不断的被递归调用
+	}
+	else
+	{
 		return arr[ptr];
 	}
-	else {
-		int lmax = getMax(arr, ptr, (ptr + len) / 2);
-		int rmax = getMax(arr, (ptr + len) / 2, len);
-		return lmax > rmax ? lmax : rmax;
-	}
+
+	return lmax > rmax ? lmax : rmax;
 }
 
 int main(void)
 {
 	int* arr;
 	int len = getIntArr(&arr);
-	printf("最大值是%d\n", getMax(arr,0,len));
+	printf("最大值是%d\n", getMax(arr, 0, len));
 
 	return 0;
 }
